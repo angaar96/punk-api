b2ebe8fe19f029edbf06c317a33184560904878f
Angaar Uriakhil <angar1996@gmail.com>
2021-11-16 15:24:53 +0000

    fixed responsiveness

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -9,8 +9,10 @@ const Navbar = (props) => {
       <form className = "navbar__form">
         <label htmlFor = "navbar__form__input" className = "navbar__form__input-label"> Curious about a Brewdog beer? </label> 
         <input placeholder="Search for a beer..." id = "navbar__form__input" name = "navbar__form__input" className = "navbar__form__input" onChange={searchForBeer}/>
-        <h2 className = "navbar__form__filters-label"> Filters: </h2> 
-        <FiltersList filterBeers = {filterBeers} />
+        <div className="navbar__form__filters">
+          <h2 className = "navbar__form__filters-label"> Filters: </h2> 
+          <FiltersList filterBeers = {filterBeers} />
+        </div>
       </form>
     </div>
   )
